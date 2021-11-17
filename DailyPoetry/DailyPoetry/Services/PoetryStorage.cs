921c69b1ef766af35293f30118d941415a216ace
liuke <anlianfengjie@hotmail.com>
2021-11-17 14:50:30 +0800

    完成诗词存储

--- DailyPoetry/DailyPoetry/Services/PoetryStorage.cs
+++ DailyPoetry/DailyPoetry/Services/PoetryStorage.cs
@@ -1,4 +1,5 @@
 ﻿using DailyPoetry.Models;
+using SQLite;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -6,26 +7,28 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 namespace DailyPoetry.Services
 {
     public class PoetryStorage : IPoetryStorage
     {
         private const string DbName = "poetrydb.sqlite3";
-        
+
+        private SQLiteAsyncConnection _connection;
+
+        public SQLiteAsyncConnection Connection => _connection ?? (_connection = new SQLiteAsyncConnection(PoetryDbPath));
+
         //数据库文件路径
         private static readonly string PoetryDbPath =
             Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DbName);
 
-        public Task<Poetry> GetPoetryAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<Poetry> GetPoetryAsync(int id) =>
+            await _connection.Table<Poetry>().FirstOrDefaultAsync(p => p.Id == id);
+        
 
-        public Task<IList<Poetry>> GetPoetryListAsync(Expression<Func<Poetry, bool>> where, int skip, int take)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<IList<Poetry>> GetPoetryListAsync(Expression<Func<Poetry, bool>> where, int skip, int take)=>
+            await _connection.Table<Poetry>().Where(where).Skip(skip).Take(take).ToListAsync();
 
         public async Task InitializeAsync()
         {
@@ -34,13 +37,25 @@ namespace DailyPoetry.Services
                 using (var dbAssertStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(DbName))
                 {
                     await dbAssertStream.CopyToAsync(dbFileStream);
-                }     
+                }
             };
+
+            Preferences.Set(PoetryStorageConstants.VersionKey, PoetryStorageConstants.Version);
         }
 
-        public bool IsInitialized()
-        {
-            throw new NotImplementedException();
-        }
+  
+
+        public bool IsInitialized() => 
+            Preferences.Get(PoetryStorageConstants.VersionKey, -1) == PoetryStorageConstants.Version;
+
     }
+
+    public static class PoetryStorageConstants
+    {
+
+        public const int Version = 1;
+        public const string VersionKey = nameof(PoetryStorageConstants) + "." + nameof(Version);
+
+    }
+
 }
